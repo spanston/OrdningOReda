c1ac1642d2cfdd5390309fde429f7fd34d182156
Johannes Chamuon <jcn16005@student.mdh.se>
2019-06-23 17:08:25 +0200

    Git problems

--- AspNetCoreTodo/Data/Migrations/20190623150750_namechanges2.Designer.cs
+++ AspNetCoreTodo/Data/Migrations/20190623150750_namechanges2.Designer.cs
@@ -4,14 +4,16 @@ using AspNetCoreTodo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AspNetCoreTodo.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190623150750_namechanges2")]
+    partial class namechanges2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
